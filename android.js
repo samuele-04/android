a64091d11a1d8e3f9d9f6e43dd48161ff685585f
samuele-04 <103112161+samuele-04@users.noreply.github.com>
2022-04-06 12:31:51 +0200

    Update android.js

--- android.js
+++ android.js
@@ -1,1 +1,17 @@
-
+if (window.innerWidth && window.innerWidth <= 600) {
+  $(document).redy(function(){
+    $('#header ul').addClass('nascondi');
+    var bloccoDaInserire=$('<div class"bottone"> <input id="ppp" type"button" value="Show" onclick="toggleMenu()"></div>');
+    bloccoDaInserire.appendTo("#header");
+    
+  });
+  function toggleMenu() {
+    if ($('#ppp').attr('value')=="Show") {
+      $('#ppp').attr('value',"hide");
+    }
+    else {
+      $('#ppp').attr('value',"Show");
+    }
+    $('#header ul').toggleClass('nascondi');
+  }
+}
